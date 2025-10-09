e4af90f2a906376538865794ced7334195a497b4
John hardy <73005810+iam-johnhardy@users.noreply.github.com>
2025-10-09 17:29:32 +0100

    fixing errors

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(),tsconfigPaths()],
-  base:"/John_Hardy_Portfolio",
+  base:"/",
   server: {
     port: 5173,
     // If you serve behind a proxy and want HMR to connect to a specific host/port:
